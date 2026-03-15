abe322cc05bb5d12dd93779f05969a6eeacea879
Kazol l Habib <themacmini@Kazol-l-Habib.local>
2026-03-15 07:59:10 +0600

    added visited not visited css

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
 const Country = ({ country }) => {
-    console.log(country)
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () =>{
+        setVisited(true)
+        setVisited(visited ? false : true)
+    }
     return (
-        <div className="country-card">
+        <div className={`country-card ${visited ? 'country-visited' : 'country-not-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
             <p>{country.population.population > 300000 ? 'Big Country' : 'Small Country'}</p>
+            <button className='country-btn' onClick={handleVisited}>{visited ? 'Visited' : 'Not Visited'}</button>
         </div>
     );
 };
